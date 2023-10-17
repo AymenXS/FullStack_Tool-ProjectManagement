3ba24fa4e918c94ca7dafe9412914ee22c490f66
Aymen <aymenghaloua@gmail.com>
2023-10-17 01:48:29 +0100

    FIX: Exposed Sensitive Data

--- server.js
+++ server.js
@@ -1,21 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+const connectDB = require('./db/connect');
+require('dotenv').config();
 const PORT = 3000;
-
-// Connect to MongoDB using Mongoose
-mongoose.connect(
-  'mongodb+srv://Aymen:3C0NbIZrXUkbG8Gn@cluster0.onx0j8e.mongodb.net/?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-// Check MongoDB connection
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
-});
 
 // Middleware to parse JSON data
 app.use(express.json());
@@ -23,6 +10,15 @@ app.use(express.json());
 // Define routes and handle database operations here
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
