39e83b7154361991e51f1395ae31a1ea5d6968b4
Aymen <aymenghaloua@gmail.com>
2023-10-17 03:12:03 +0100

    UPDATING: Models

--- models/Project.js
+++ models/Project.js
@@ -1,23 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Mongoose Schema
-const userSchema = new Schema({
-  name: String,
-  email: String,
+const ProjectSchema = new Schema({
+  label: String,
+  description: String,
+  status: Boolean,
+  starting_date: Date,
+  ending_date: Date,
 });
 
-// Mongoose Model
-const User = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Project', ProjectSchema)
 
-// Express.js Routes
-app.post('/users', async (req, res) => {
-  try {
-    const { name, email } = req.body;
-    const newUser = new User({ name, email });
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
--- models/Task.js
+++ models/Task.js
@@ -1,23 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Mongoose Schema
-const userSchema = new Schema({
-  name: String,
-  email: String,
+const TaskSchema = new Schema({
+  label: String,
+  description: String,
+  project: { type: Schema.Types.ObjectId, ref: 'Project' }, // To be tackled later!
+  status: Boolean,
+  starting_date: Date,
+  ending_date: Date,
 });
 
-// Mongoose Model
-const User = mongoose.model('User', userSchema);
-
-// Express.js Routes
-app.post('/users', async (req, res) => {
-  try {
-    const { name, email } = req.body;
-    const newUser = new User({ name, email });
-    await newUser.save();
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+module.exports = mongoose.model('Project', TaskSchema);
